8871150feb3b7cffdf12a76e70713b870b55216a
ChuckRIHead <chuckhead@gmail.com>
2019-04-19 14:07:26 -0700

    updated logo

--- jupyter_codeserver_proxy/__init__.py
+++ jupyter_codeserver_proxy/__init__.py
@@ -17,7 +17,7 @@ def setup_codeserver():
         if working_dir is None:
             working_dir = os.getenv("JUPYTER_SERVER_ROOT", ".")
 
-        return [full_path, working_dir, '--port=' + str(port), "--no-auth", " --disable-telemetry"  ]
+        return [full_path, working_dir, '--port=' + str(port), "--no-auth", " --disable-telemetry", "--allow-http"  ]
         
     return {
         'command': _codeserver_command,
