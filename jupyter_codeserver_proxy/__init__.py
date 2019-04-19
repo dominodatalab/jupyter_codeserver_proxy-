7d51145512a7a61194b13f7662da5f4abac6ffdb
ChuckRIHead <chuckhead@gmail.com>
2019-04-19 09:45:55 -0700

    changing cmd

--- jupyter_codeserver_proxy/__init__.py
+++ jupyter_codeserver_proxy/__init__.py
@@ -17,8 +17,8 @@ def setup_codeserver():
         if working_dir is None:
             working_dir = os.getenv("JUPYTER_SERVER_ROOT", ".")
 
-        return [full_path, working_dir, '--port=' + str(port), "--allow-http", "--no-auth", "--disable-telemetry", "-e /opt/code-server/extensions/" ]
-
+        return [full_path, working_dir, '--port=' + str(port), "--no-auth", " -e /opt/code-server/extensions/", " --disable-telemetry"  ]
+        
     return {
         'command': _codeserver_command,
         'timeout': 20,
